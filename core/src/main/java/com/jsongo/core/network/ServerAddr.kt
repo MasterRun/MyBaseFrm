55860b51387b84f86020da4a6a5cbf0914719b85
jsongo <jsongo97@outlook.com>
2019-10-15 23:58:35 +0800

    加载图片测试

--- core/src/main/java/com/jsongo/core/network/ServerAddr.kt
+++ core/src/main/java/com/jsongo/core/network/ServerAddr.kt
@@ -14,8 +14,9 @@ object ServerAddr {
     val SERVER_ADDRESS_RELEASE: String = BaseCore.context.getString(R.string.SERVER_ADDRESS_RELEASE)
 
     /**  测试服务器地址  */
-    val SERVER_ADDRESS_DEBUG = BaseCore.context.getString(R.string.SERVER_ADDRESS_DEBUG)
+    val SERVER_ADDRESS_DEBUG: String = BaseCore.context.getString(R.string.SERVER_ADDRESS_DEBUG)
 
     /** 服务器域名  */
-    val SERVER_ADDRESS = if (BaseCore.isDebug) SERVER_ADDRESS_DEBUG else SERVER_ADDRESS_RELEASE
+    val SERVER_ADDRESS: String =
+        if (BaseCore.isDebug) SERVER_ADDRESS_DEBUG else SERVER_ADDRESS_RELEASE
 }
--- demo/src/main/java/com/jsongo/mybasefrm/view/activity/Main2Activity.kt
+++ demo/src/main/java/com/jsongo/mybasefrm/view/activity/Main2Activity.kt
@@ -3,18 +3,15 @@ package com.jsongo.mybasefrm.view.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
-import android.view.View
-import android.view.ViewTreeObserver
 import com.jsongo.ajs.helper.AjsWebViewHost
-import com.jsongo.ajs.webloader.AJsWebLoader
+import com.jsongo.ajs.webloader.AJsWebPage
 import com.jsongo.annotation.anno.Page
 import com.jsongo.core.mvp.base.BaseActivity
+import com.jsongo.core.network.ServerAddr
 import com.jsongo.mybasefrm.R
 import com.jsongo.ui.component.image.banner.lib.anim.select.ZoomInEnter
 import com.jsongo.ui.component.image.banner.lib.transform.ZoomOutSlideTransformer
 import com.jsongo.ui.component.image.banner.widget.bean.BannerItem
-import com.qmuiteam.qmui.util.QMUIDisplayHelper
-import com.vondear.rxtool.view.RxToast
 import kotlinx.android.synthetic.main.activity_main2.*
 
 @Page(R.layout.activity_main2, 0)
@@ -32,64 +29,36 @@ class Main2Activity : BaseActivity(), AjsWebViewHost {
     }
 
     private fun initWebLoador() {
-//        val webPath = "file:///android_asset/web/index.html"
-        val webPath = "https://www.baidu.com"
 
-        //使用 ajswebloader
-        val ajswebloader = ajswebloader_flat_card
-        if (ajswebloader is AJsWebLoader) {
-            ajswebloader.webPath = webPath
-            ajswebloader.topbar.visibility = View.GONE
-        }
-        //调整高度
-        flat_card.viewTreeObserver.addOnGlobalLayoutListener(object :
-            ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                val _250dp2px = QMUIDisplayHelper.dp2px(this@Main2Activity, 250)
-                if (flat_card.height > _250dp2px) {
-                    flat_card.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                    val layoutParams = flat_card.layoutParams
-                    layoutParams.height = _250dp2px
-                    flat_card.layoutParams = layoutParams
-                }
-            }
-        })
+        val serverAddress = ServerAddr.SERVER_ADDRESS
 
         //使用ajswebview
-        card_webloader.url = "file:///android_asset/web/index.html"
-        card_webloader.scrollable = true
+//        card_webloader.url = "file:///android_asset/web/index.html"
+        card_webloader.url = "${serverAddress}image/others/banner/img_banner_4.jpg"
         card_webloader.ajsWebViewHost = this
         card_webloader.initLoad()
+        card_webloader2.url = "${serverAddress}image/others/banner/img_banner_3.jpg"
         card_webloader2.ajsWebViewHost = this
         card_webloader2.initLoad()
+        card_webloader3.url = "${serverAddress}image/others/banner/img_banner_5.jpg"
+        card_webloader3.ajsWebViewHost = this
+        card_webloader3.initLoad()
     }
 
     private fun initImageBanner() {
-        val items =
-            arrayListOf(
-                BannerItem(
-//                        "http://pic26.nipic.com/20121221/9252150_142515375000_2.jpg",
-                    "/storage/emulated/0/ADM/face1.jpg",
-                    "这是图片1"
-                ),
-                BannerItem(
-                    "http://img5.imgtn.bdimg.com/it/u=3300305952,1328708913&fm=26&gp=0.jpg",
-//                        "http://pic39.nipic.com/20140307/13928177_195158772185_2.jpg",
-                    "这是图片2"
-                ),
-                BannerItem(
-                    "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=508387608,2848974022&fm=27&gp=0.jpg",
-//                        "http://pic27.nipic.com/20130321/9678987_225139671149_2.jpg",
-                    "这是图片3"
-                )
-            )
+        val serverAddress = ServerAddr.SERVER_ADDRESS
+        val items = arrayListOf(
+            BannerItem("${serverAddress}image/others/banner/img_banner_1.jpg", "我是江小白"),
+            BannerItem("${serverAddress}image/others/banner/img_banner_2.jpg", "画江湖之不良人"),
+            BannerItem("${serverAddress}image/others/banner/img_banner_8.jpg", "罗小黑战记")
+        )
         simple_ib.setSelectAnimClass(ZoomInEnter::class.java)//set indicator select anim
             .setSource(items)//data source list
             .setTransformerClass(ZoomOutSlideTransformer::class.java)//set page transformer
             .startScroll()
         simple_ib.setOnItemClickListener {
             val item = items.get(it)
-            RxToast.info(item.toString())
+            AJsWebPage.load(item.imgUrl)
         }
     }
 
@@ -101,6 +70,7 @@ class Main2Activity : BaseActivity(), AjsWebViewHost {
     override fun onIPageDestroy() {
         card_webloader.destroy()
         card_webloader2.destroy()
+        card_webloader3.destroy()
         super.onIPageDestroy()
     }
 
