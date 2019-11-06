b2dbff30567a9bd673c39339eee9eddc7217cf47
jsongo <jsongo97@outlook.com>
2019-11-06 23:10:21 +0800

    提交.gitignore 提交native-control

---
+++ demo/src/main/assets/web/demo1/js/native-control.js
@@ -0,0 +1,7 @@
+function onEnvReady() {
+    // ajs.topbar.statusbar(0)
+    // ajs.common.load({
+    //     url: "file:///android_asset/web/demo1/index.html",
+    //     showTopBar: false,
+    // })
+}
