df972749f315534574517327b9612bad2c08d613
jsongo <jsongo97@outlook.com>
2019-11-24 15:05:42 +0800

    登录页布局

---
+++ demo/src/main/java/com/jsongo/mybasefrm/ui/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.jsongo.mybasefrm.ui.login
+
+import com.jsongo.annotation.anno.Page
+import com.jsongo.core.arch.BaseActivity
+import com.jsongo.mybasefrm.R
+
+/**
+ * @author ： jsongo
+ * @date ： 2019/11/24 12:38
+ * @desc : 登录页
+ */
+@Page(R.layout.activity_login, 0)
+class LoginActivity : BaseActivity() {
+}
