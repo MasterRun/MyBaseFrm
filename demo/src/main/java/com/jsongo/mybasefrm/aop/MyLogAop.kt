ef99201ec9466913f36b0ed31656e0511c2a09f0
jsongo <jsongo97@outlook.com>
2019-11-24 12:18:34 +0800

    完善MyLogAop 注释

--- demo/src/main/java/com/jsongo/mybasefrm/aop/MyLogAop.kt
+++ demo/src/main/java/com/jsongo/mybasefrm/aop/MyLogAop.kt
@@ -1,17 +1,24 @@
 package com.jsongo.mybasefrm.aop
 
+import com.safframework.log.L
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
+import org.aspectj.lang.reflect.MethodSignature
+
 /**
  * @author ： jsongo
  * @date ： 19-9-22 下午11:26
  * @desc :
  */
+//标识类为切面
 //@Aspect
 class MyLogAop {
 
     /**
-     * 定义切点，标记切点为所有被@AopOnclick注解的方法
-     * 注意：这里@com.freak.kotlinhttpmanager.aop.AopOnclick需要替换成
-     * 你自己项目中AopOnclick这个类的全路径
+     * 定义切点
      */
     //@Pointcut("execution(* com.jsongo.mybasefrm.view..*.onC*(..))")  //  --  ok
     //@Pointcut("execution(* com.jsongo.mybasefrm.presenter.DemoPresenter.*(..))")
@@ -20,29 +27,43 @@ class MyLogAop {
     //@Pointcut("execution(* com.jsongo.mybasefrm.presenter..*(..))")
     //@Pointcut("execution(android.view.View.OnClickListener.onClick(..))")
 //    @Pointcut("execution(* com.jsongo.core.mvp.base.BaseActivity.onCreate(..))")  // -- ok
-    /*@Pointcut("execution(* android.app.Activity.on**(..))")  // --  ok
+/*    @Pointcut("execution(* android.app.Activity.on**(..))")  // --  ok
     fun methodLog() {
     }*/
 
 
     /**
-     * 定义一个切面方法，包裹切点方法
+     * 定义一个切面方法，包裹切点
      */
-//@Around("execution(* android.app.Activity.on**(..))")
     /*@Around("methodLog()")
     @Throws(Throwable::class)
     fun aroundJoinPoint(joinPoint: ProceedingJoinPoint): Any? {
+        //获取方法
         val methodSignature = joinPoint.signature as MethodSignature
+        //方法所在类名
         val className = methodSignature.declaringType.simpleName
+        //方法名
         val methodName = methodSignature.name
+        //方法参数名
         val parameterNames = methodSignature.parameterNames
+        //方法参数类型
         val parameterTypes = methodSignature.parameterTypes
+
+        //切点（方法）参数
         val args = joinPoint.args
+        //切点类型（方法）
         val kind = joinPoint.kind
+        //切点（方法）源码位置
         val sourceLocation = joinPoint.sourceLocation
+
+        //打印日志
         L.w("before --  ${className}#${methodName}")
+        //执行方法
         val proceed = joinPoint.proceed()
+        //打印日志
         L.w("after --  ${className}#${methodName}")
+
+        //返回执行结果
         return proceed
     }*/
 
